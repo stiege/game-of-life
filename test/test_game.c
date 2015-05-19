61aa7ae17a7f9a267714ec81a1bf6337b239f44c
Alex <alex.hodge@gmail.com>
2015-05-20 11:19:04 +1200

    Added second test

--- test/test_game.c
+++ test/test_game.c
@@ -18,3 +18,15 @@ void test_empty_size_3_gameboard_shows_blank(void)
         "   \n", GME_show(game_board)
         );
 }
+
+void test_empty_size_5_gameboard_shows_blank(void)
+{
+    struct game_board_t * game_board = GME_init(5);
+    TEST_ASSERT_EQUAL_STRING(
+        "     \n"
+        "     \n"
+        "     \n"
+        "     \n"
+        "     \n", GME_show(game_board)
+        );
+}
