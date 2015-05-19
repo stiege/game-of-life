abfbd718361bf96a69d4df3f990fb96cfa5341cd
Alex <alex.hodge@gmail.com>
2015-05-20 11:33:05 +1200

    Passing second test - Variable init board sizes

--- src/game.c
+++ src/game.c
@@ -1,11 +1,34 @@
 #include "game.h"
 #include "stdio.h"
+#include "stdlib.h"
 
-struct game_board_t * GME_init(unsigned int size)
+struct game_board_t
 {
-    return NULL;
+    unsigned int size;
+};
+
+struct game_board_t * GME_init(unsigned int _size)
+{
+    struct game_board_t * game_board = malloc(sizeof(struct game_board_t));
+    game_board->size = _size;
+    return game_board;
 }
+
 char * GME_show(struct game_board_t * board)
 {
-    return "   \n   \n   \n";
+    unsigned int string_size = board->size * (board->size + 2 ) + 1;
+    char * result = malloc(string_size);
+    char * current = result;
+    for (int i = 0; i < board->size; ++i)
+    {
+        for (int j = 0; j < board->size; ++j)
+        {
+            *current++ = ' ';
+
+        }
+        *current++ = '\n';
+    }
+    *current = '\0';
+
+    return result;
 }
