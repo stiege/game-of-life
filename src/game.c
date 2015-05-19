f0e3c9fd7f8fc395f2395f3268997bd2de8f9ccb
Alex <alex.hodge@gmail.com>
2015-05-20 11:15:17 +1200

    Compiling with first test

--- src/game.c
+++ src/game.c
@@ -1,1 +1,11 @@
 #include "game.h"
+#include "stdio.h"
+
+struct game_board_t * GME_init(unsigned int size)
+{
+    return NULL;
+}
+char * GME_show(struct game_board_t * board)
+{
+    return NULL;
+}
--- src/game.h
+++ src/game.h
@@ -1,5 +1,7 @@
 #ifndef game_H
 #define game_H
 
+struct game_board_t * GME_init(unsigned int size);
+char * GME_show(struct game_board_t * board);
 
 #endif // game_H
