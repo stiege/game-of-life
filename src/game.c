a28a58cb31a040653a0a38372c8838f32b2bd180
Alex <alex.hodge@gmail.com>
2015-05-20 13:54:27 +1200

    Passing set test correctly

--- src/game.c
+++ src/game.c
@@ -1,42 +1,44 @@
 #include "game.h"
 #include "stdio.h"
 #include "stdlib.h"
+#include "strings.h"
 
 struct game_board_t
 {
     unsigned int size;
+    char * board_string;
 };
 
 struct game_board_t * GME_init(unsigned int _size)
 {
+    unsigned int string_size = _size * (_size + 1 ) + 1;
     struct game_board_t * game_board = malloc(sizeof(struct game_board_t));
+    game_board->board_string = malloc(string_size * sizeof(char));
+    memset(game_board->board_string,' ',string_size * sizeof(char));
+    unsigned int offset = _size;
+    for (int i = 0; i < _size; ++i)
+    {
+        game_board->board_string[offset] = '\n';
+        offset += _size + 1;
+    }
+    offset -= _size;
+
+    game_board->board_string[offset] = '\0';
     game_board->size = _size;
     return game_board;
 }
 
 char * GME_show(struct game_board_t * board)
 {
-    unsigned int string_size = board->size * (board->size + 2 ) + 1;
-    char * result = malloc(string_size);
-    char * current = result;
-    for (int i = 0; i < board->size; ++i)
-    {
-        for (int j = 0; j < board->size; ++j)
-        {
-            *current++ = ' ';
-        }
-        *current++ = '\n';
-    }
-    *current = '\0';
-
-    return result;
+    return board->board_string;
 }
 
 void GME_set(struct game_board_t * board,
     unsigned int x,
     unsigned int y)
 {
-
+    unsigned int offset = x + y * (board->size + 1);
+    board->board_string[offset] = 'o';
 }
 void GME_iterate(struct game_board_t * board)
 {
