dc071fcdbca44a05dd14b203fa35115bfdd98dc5
Alex <alex.hodge@gmail.com>
2015-05-24 08:43:43 +1200

    Persistent block remains

--- test/test_game.c
+++ test/test_game.c
@@ -79,3 +79,25 @@ void test_single_cell_dies(void)
     GME_game_board_dtor(game_board);
     TEST_ASSERT_EQUAL_INT(mdata_before.chunks_used, mstats().chunks_used);
 }
+
+void test_pair_remains(void)
+{
+    struct game_board_t * game_board = GME_init(3);
+    GME_set(game_board, (struct point_t) {.x = 1, .y = 1});
+    GME_set(game_board, (struct point_t) {.x = 1, .y = 2});
+    GME_set(game_board, (struct point_t) {.x = 0, .y = 1});
+    GME_set(game_board, (struct point_t) {.x = 0, .y = 2});
+    TEST_ASSERT_EQUAL_STRING(
+        "   \n"
+        "oo \n"
+        "oo \n", GME_show(game_board)
+        );
+    GME_iterate(game_board);
+    TEST_ASSERT_EQUAL_STRING(
+        "   \n"
+        "oo \n"
+        "oo \n", GME_show(game_board)
+        );
+    GME_game_board_dtor(game_board);
+    TEST_ASSERT_EQUAL_INT(mdata_before.chunks_used, mstats().chunks_used);
+}
