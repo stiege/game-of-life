c1bab6068ae75eadb57b681ec8f76e70a30b8de9
Alex <alex.hodge@gmail.com>
2015-05-23 18:23:14 +1200

    Cell module can return underpopulated cells

--- src/cell.c
+++ src/cell.c
@@ -10,7 +10,14 @@ struct cell_list_t
     unsigned int length;
 };
 
-static bool cell_is_underpopulated(struct cell_t cell);
+static bool cell_is_underpopulated (
+    struct cell_list_t * cell_list,
+    struct cell_t cell);
+static unsigned int living_neighbours (
+    struct cell_list_t * cell_list,
+    struct cell_t cell);
+static bool cells_are_close(struct cell_t cell_a, struct cell_t cell_b);
+static bool uints_are_close(unsigned int a, unsigned int b);
 
 cell_list_t * CELL_list_from_string(char const * string)
 {
@@ -96,32 +103,92 @@ cell_list_t * CELL_filter_for_underpopulated(cell_list_t * cells)
         unsigned int underpopulated_count = 0;
         for (unsigned int a = 0; a < cells->length; a++)
         {
-            if (cell_is_underpopulated(cells->list[a]))
+            if (cell_is_underpopulated(cells,cells->list[a]))
             {
                 underpopulated_count++;
             }
         }
 
-        cell_list_t * underpopulated_cells = 
+        cell_list_t * underpopulated_cells = malloc(sizeof(cell_list_t));
+        underpopulated_cells->list = 
             malloc(sizeof(struct cell_t) * underpopulated_count);
+        underpopulated_cells->length = 0;
 
-        underpopulated_count = 0;
         for (unsigned int a = 0; a < cells->length; a++)
         {
-            if (cell_is_underpopulated(cells->list[a]))
+            if (cell_is_underpopulated(cells,cells->list[a]))
             {
-                underpopulated_cells->list[underpopulated_count] 
+                underpopulated_cells->list[underpopulated_cells->length] 
                     = cells->list[a];
-                underpopulated_count++;
+                underpopulated_cells->length++;
             }
         }
-
+        ret_val = underpopulated_cells;
     }
 
     return ret_val;
 }
 
-static bool cell_is_underpopulated(struct cell_t cell)
+static bool cell_is_underpopulated(struct cell_list_t * cell_list,
+    struct cell_t cell)
 {
-    return false;
+    bool ret_val = false;
+    if (3 > living_neighbours(cell_list,cell))
+    {
+        ret_val = true;
+    }
+    return ret_val;
+}
+
+static unsigned int living_neighbours(struct cell_list_t * cell_list,
+    struct cell_t cell)
+{
+    unsigned int ret_val = 0;
+    for (unsigned int a = 0; a < cell_list->length; a++)
+    {
+        if (cells_are_close(cell, cell_list->list[a]))
+        {
+            ret_val++;
+        }
+    }
+    return ret_val;
+}
+
+static bool cells_are_close(struct cell_t cell_a, struct cell_t cell_b)
+{
+    bool ret_val = false;
+    if (    uints_are_close(cell_a.x, cell_b.x) 
+        &&  uints_are_close(cell_a.y,cell_b.y)
+        &&  !(memcmp(&cell_a, &cell_b, sizeof(struct cell_t)))
+        )
+    {//close but not the same
+        ret_val = true;
+    }
+
+    return ret_val;
+}
+
+static bool uints_are_close(unsigned int a, unsigned int b)
+{
+    unsigned int greatest = 0;
+    unsigned int least = 0;
+    bool ret_val = false;
+
+    if (a <= b)
+    {
+        greatest = b;
+        least = a;
+    }
+    else
+    {
+        greatest = a;
+        least=b;
+    }
+
+    if (greatest - least < 2)
+    {
+        ret_val = true;
+    }
+
+    return ret_val;
 }
